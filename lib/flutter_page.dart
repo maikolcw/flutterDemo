02ce64e986f41c4afa1ede0f71303b3b658ffb8a
maikolcw <whysoraccoony@gmail.com>
2023-06-18 19:54:36 -0700

    added several button types and row widget with gesture detector

--- lib/flutter_page.dart
+++ lib/flutter_page.dart
@@ -33,15 +33,62 @@ class _FlutterPageState extends State<FlutterPage> {
           const Divider(
             color: Colors.black,
           ),
+          // a div
           Container(
+            margin: const EdgeInsets.all(7.0),
+            padding: const EdgeInsets.all(7.0),
             color: Colors.blue,
-            child: const Text(
-              "This is a Text Widget",
-              style: TextStyle(
-                color: Colors.white,
+            // maximum width
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                "This is a Text Widget",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint("You pressed the Elevated Button");
+            },
+            child: const Text("This is an Elevated Button"),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint("You pressed the Outlined button");
+            },
+            child: const Text("This is an Outlined Button"),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint("You pressed the Text button");
+            },
+            child: const Text("This is an Text Button"),
+          ),
+          // sample event handler 
+          GestureDetector(
+            // activates taps anywhere on Row Widget
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint("You click on Widgets in the Row Widget");
+            },
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.blue,
+                ),
+                Text("This is a Row Widget"),
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
