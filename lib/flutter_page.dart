54f653f1873af0189ebc4fbc14d60ff17e7a6664
maikolcw <whysoraccoony@gmail.com>
2023-06-18 20:21:41 -0700

    added image widget via network

--- lib/flutter_page.dart
+++ lib/flutter_page.dart
@@ -116,7 +116,8 @@ class _FlutterPageState extends State<FlutterPage> {
                   isCheckBox = newBool;
                 });
               },
-            )
+            ),
+            Image.network("https://storage.googleapis.com/cms-storage-bucket/c823e53b3a1a7b0d36a9.png")
           ],
         ),
       ),
